3d0763e7cdd35fef6e266141482b2d13afa3ef1c
Andre <164786540+Bluepath-null@users.noreply.github.com>
2026-01-14 23:02:20 -0300

    fix: estrutura correta para o deploy no Render

--- db/connection.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("./database.db", (err) => {
-  if (err) console.error(err);
-  else console.log("🗄️ Banco conectado");
-});
-
-
-module.exports = db;
